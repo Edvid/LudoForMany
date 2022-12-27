d4f205674ca2fa4829de0283ebfc6eadc257f277
Edvid <spacemastr@gmail.com>
2022-12-27 19:29:40 +0100

    the homes are coded in now

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -75,9 +75,53 @@ class Board extends React.Component {
         <p><em>Insert a number larger than 2...</em></p>
         );
       } else {
+        
+        let ret = [];
+
+        for(let i = 0; i < numberOfPlayers; i++){
+          let x1: number = Math.sin(i*2*Math.PI/numberOfPlayers);
+          let y1: number = Math.cos(i*2*Math.PI/numberOfPlayers);
+
+          let x2: number= Math.sin((i+1)*2*Math.PI/numberOfPlayers);
+          let y2: number= Math.cos((i+1)*2*Math.PI/numberOfPlayers);
+
+          if(i == 0){
+            const circleFraction: number = Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
+            const armLength: number = circleFraction * 3.5;
+            const circleRatio: number = 1.0/(1.0 + armLength);
+            circleSize = circleRatio * imageSize/2.0;
+          }
+
+          x1 *= circleSize;
+          y1 *= circleSize;
+          x2 *= circleSize;
+          y2 *= circleSize;
+          sidelength = Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
+
+          const offset: number = imageSize/2;
+
+          
+          x1 += offset;
+          y1 += offset;
+          x2 += offset;
+          y2 += offset;
+          
+          
+          const points: string = 
+          offset.toString().replace(",", ".") + "," + offset.toString().replace(",", ".") + " " + 
+          x1.toString().replace(",", ".")  + "," + y1.toString().replace(",", ".") + " " + 
+          x2.toString().replace(",", ".")  + "," + y2.toString().replace(",", ".") + " " + 
+          offset.toString().replace(",", ".") + "," + offset.toString().replace(",", ".");
+
+          ret.push(
+                <polygon points={points} style={{fill: colors[i % colors.length], stroke: "black", strokeWidth: 1}}/>
+          );
+        }
+
         return (
           <svg width={imageSize} height={imageSize} xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
-        </svg>
+            {ret}
+          </svg>
         );
       }
   }
