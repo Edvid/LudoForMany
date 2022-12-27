3e08f0459266c41c0ee531116d003989ef2f32fc
Edvid <spacemastr@gmail.com>
2022-12-27 15:08:38 +0100

    mostly whitespace

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,59 +2,58 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-let numberOfPlayers: number //bound
+let numberOfPlayers: number; //bound
 
-function numberOfPlayersChangeHandler(event: any){
-  numberOfPlayers = event.target.value;
+function numberOfPlayersChangeHandler(event: any) {
+  numberOfPlayers = event.target.value as number;
 }
 
 let sidelength: number = 0;
 let imageSize: number = 4000;
 let circleSize: number = 1;
 
-  let colors: string[] = [
-    "blue",
-    "Firebrick",
-    "lime",
-    "BlueViolet",
-    "HotPink",
-    "DodgerBlue",
-    "Silver",
-    "SaddleBrown"
+let colors: string[] = [
+  "blue",
+  "Firebrick",
+  "lime",
+  "BlueViolet",
+  "HotPink",
+  "DodgerBlue",
+  "Silver",
+  "SaddleBrown"
 ];
 let shapeCount: number = 16;
 
 function App() {
   return (
-    
-    <div className="App">
-          
-    <h1>Ludo Generator</h1>
 
-    <p>Put in a number of players, and a board will be generated</p>  
-      
-    <input type="number" value={numberOfPlayers} onChange={numberOfPlayersChangeHandler}/><br/>
-    <br/>
-    <br/>
-    
-    <Builder></Builder>
+    <div className="App">
+
+      <h1>Ludo Generator</h1>
+
+      <p>Put in a number of players, and a board will be generated</p>
+
+      <input type="number" value={numberOfPlayers} onChange={numberOfPlayersChangeHandler} /><br />
+      <br />
+      <br />
+
+      <Builder></Builder>
     </div>
   );
 }
 
-function Builder(){
-  if (numberOfPlayers <= 2)
-  {
+function Builder() {
+  if (numberOfPlayers <= 2) {
     return (
       <p><em>Insert a number larger than 2...</em></p>
-    );  
-  }else{
+    );
+  } else {
     return (
-        <svg width={imageSize} height={imageSize} xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
-        </svg>
-      );  
+      <svg width={imageSize} height={imageSize} xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+      </svg>
+    );
   }
-  
+
 }
 
 export default App;
