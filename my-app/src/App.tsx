a5a330b8e239b7740ce71bbfbd253ef8bf5a63a5
Edvid <spacemastr@gmail.com>
2022-12-27 19:53:06 +0100

    more of it

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -107,7 +107,7 @@ class Board extends React.Component {
           y2 += offset;
           
           
-          const points: string = 
+          let points: string = 
           offset.toString().replace(",", ".") + "," + offset.toString().replace(",", ".") + " " + 
           x1.toString().replace(",", ".")  + "," + y1.toString().replace(",", ".") + " " + 
           x2.toString().replace(",", ".")  + "," + y2.toString().replace(",", ".") + " " + 
@@ -116,6 +116,57 @@ class Board extends React.Component {
           ret.push(
                 <polygon points={points} style={{fill: colors[i % colors.length], stroke: "black", strokeWidth: 1}}/>
           );
+
+          const cellX: number = (x1 - x2) / 3;
+          const cellY: number = (y1 - y2) / 3; 
+
+          for(var j = 0; j < 9; j++){
+              for(var k = 0; k < 3; k++){
+                  
+                  points = 
+                  (x2+k*cellX+j*cellY).toString().replace(",", ".") + "," + (y2+k*cellY+j*-cellX).toString().replace(",", ".") + " " + 
+                  (x2+(k+1)*cellX+j*cellY).toString().replace(",", ".") + "," + (y2+(k+1)*cellY+j*-cellX).toString().replace(",", ".") + " " + 
+                  (x2+(k+1)*cellX+(j+1)*cellY).toString().replace(",", ".") + "," + (y2+(k+1)*cellY+(j+1)*-cellX).toString().replace(",", ".") + " " + 
+                  (x2+k*cellX+(j+1)*cellY).toString().replace(",", ".") + "," + (y2+k*cellY+(j+1)*-cellX).toString().replace(",", ".");
+                  
+                  if(j < 6){
+                      if((k == 1 && j!=5) || (k == 2 && j == 4)){
+                        ret.push(
+                            <polygon points={points} style={{fill: colors[i % colors.length], stroke: "black", strokeWidth: 1}}/>
+                          );
+                      }else{
+                        ret.push(
+                            <polygon points={points} style={{fill: "white", stroke: "black", strokeWidth: 1}}/>
+                          );
+                      }
+                      const ics: number = (sidelength / 5.0);
+                      const insideX: string = ((x2+(k+0.5)*cellX+(j+0.5)*cellY)-ics/2.0).toString().replace(",", ".");
+                      const insideY: string = ((y2+(k+0.5)*cellY+(j+0.5)*-cellX)-ics/2.0).toString().replace(",", ".");
+                      const iconSize: string = ics.toString().replace(",", ".");
+                      
+                      if((k == 2 && j == 4) || (k == 0 && j == 3)){ /* Globe */
+                          ret.push(
+                            <image href="images/Simple_Globe.svg" x={insideX} y={insideY} width={iconSize} height={iconSize}></image>
+                          );
+                      }else if((k == 0 && j == 0) || (k == 1 && j == 5)){ /* star */
+                          ret.push(
+                            <image href="images/star_pictogram.png" x={insideX} y={insideY} width={iconSize} height={iconSize}></image>
+                          );
+                      }
+                  }else if(k % 2 == 0 && j % 2 == 0){
+                      const insideX: number = (x2+(k+0.5)*cellX+(j+1)*cellY);
+                      const insideY: number = (y2+(k+0.5)*cellY+(j+1)*-cellX);
+                      const radius: number = sidelength/6;
+
+                      ret.push(
+                        <circle cx={insideX.toString().replace(",", ".")} cy={insideY.toString().replace(",", ".")} r={radius.toString().replace(",", ".")} style={{fill: colors[i % colors.length], stroke: "black", strokeWidth: 1}}/>
+                      );
+
+                  }
+
+                  
+              }
+          }
         }
 
         return (
